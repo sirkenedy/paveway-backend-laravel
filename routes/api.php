52ee7e863b4f3f814dcd91c9bd28b6fdd9f7fa89
Sirkenedy <owoputikehinde@gmail.com>
2020-01-31 15:47:40 +0100

    create a new test file for BookController

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('role','Api\RoleController@store');
+Route::post('role', 'Api\RoleController@store');
 // Route::get('user','Api\UserController@index');
 
 
@@ -28,31 +28,28 @@ Route::post('role','Api\RoleController@store');
  * forgot password, email verification,...
  */
 
-Route::post('auth/signup','Api\AuthController@signup');  //register a new User
-Route::post('auth/signin','Api\AuthController@signIn');  //authenticate a user and send back a token
-Route::post('auth/forgot','Api\AuthController@forgotPassword');   //user forgot password
+Route::post('auth/signup', 'Api\AuthController@signup');  //register a new User
+Route::post('auth/signin', 'Api\AuthController@signIn');  //authenticate a user and send back a token
+Route::post('auth/forgot', 'Api\AuthController@forgotPassword');   //user forgot password
 
- /** AuthContoller endpoints ends here */
- Route::group( ['middleware'=> 'jwt.auth'], function() {
-    Route::apiresource('book','Api\BookController');
-    Route::put('book/file/{id}','Api\BookController@updateBookFile');
-    Route::get('userdetails','Api\UserController@getUserDetails');
+/** AuthContoller endpoints ends here */
+Route::group(['middleware' => 'jwt.auth'], function () {
+    Route::apiresource('book', 'Api\BookController');
+    Route::put('book/file/{id}', 'Api\BookController@updateBookFile');
+    Route::get('userdetails', 'Api\UserController@getUserDetails');
 });
 
-Route::apiresource('faculty','Api\FacultyController');
-Route::get('faculty/{id}/dep','Api\FacultyController@departments');
+Route::apiresource('faculty', 'Api\FacultyController');
+Route::get('faculty/{id}/dep', 'Api\FacultyController@departments');
 
 
-Route::apiresource('departments','Api\DepartmentController');
-Route::get('departments/{id}/prog','Api\DepartmentController@programs');
+Route::apiresource('departments', 'Api\DepartmentController');
+Route::get('departments/{id}/prog', 'Api\DepartmentController@programs');
 
 
-Route::apiresource('programs','Api\ProgramController');
+Route::apiresource('programs', 'Api\ProgramController');
 
-Route::get('levels','Api\LevelController@index');
+Route::get('levels', 'Api\LevelController@index');
 
-Route::post('levels','Api\LevelController@store');
-Route::delete('levels/{id}','Api\LevelController@destroy');
-
-
- 
+Route::post('levels', 'Api\LevelController@store');
+Route::delete('levels/{id}', 'Api\LevelController@destroy');
